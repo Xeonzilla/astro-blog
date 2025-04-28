d02ac37623beeb879deb55e92b3ac9edb3ddf332
Xeonzilla <xeonrao01@gmail.com>
2025-04-28 22:36:34 +0800

    chore: Merge from upstream
    Fix TOC
    
    Fix search panel style
    
    Remove astro-compress
    
    Update config
    
    Update dependencies
    
    Reference: [Upstream#402](https://redirect.github.com/saicaca/fuwari/pull/402) [Upstream#302](https://redirect.github.com/saicaca/fuwari/pull/302) [Upstream#295](https://redirect.github.com/saicaca/fuwari/pull/295) [Upstream#424](https://redirect.github.com/saicaca/fuwari/pull/424) [Upstream#428](https://redirect.github.com/saicaca/fuwari/pull/428)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import swup from "@swup/astro";
-import Compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
@@ -21,133 +20,130 @@ import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 export default defineConfig({
-  site: "https://xeonzilla.top/",
-  base: "/",
-  trailingSlash: "always",
-  redirects: {
-    '/post/[...slug]': '/posts/[...slug]',
-  },
-  adapter: cloudflare(),
-  integrations: [
-    tailwind(
-        {
-          nesting: true,
-        }
-    ),
-    swup({
-      theme: false,
-      animationClass: "transition-swup-",
-      containers: ["main", "#toc"],
-      smoothScrolling: true,
-      cache: true,
-      preload: true,
-      accessibility: true,
-      updateHead: true,
-      updateBodyClass: false,
-      globalInstance: true,
-    }),
-    icon({
-      include: {
-        "preprocess: vitePreprocess(),": ["*"],
-        "fa6-brands": ["*"],
-        "fa6-regular": ["*"],
-        "fa6-solid": ["*"],
-      },
-    }),
-    svelte(),
-    sitemap(),
-    Compress({
-      CSS: false,
-      Image: false,
-      Action: {
-        Passed: async () => true,
-      },
-    }),
-  ],
-  markdown: {
-    remarkPlugins: [
-      remarkMath,
-      remarkReadingTime,
-      remarkExcerpt,
-      remarkGithubAdmonitionsToDirectives,
-      remarkDirective,
-      remarkSectionize,
-      parseDirectiveNode,
-    ],
-    rehypePlugins: [
-      rehypeKatex,
-      rehypeSlug,
-      [
-        rehypeComponents,
-        {
-          components: {
-            github: GithubCardComponent,
-            note: (x, y) => AdmonitionComponent(x, y, "note"),
-            tip: (x, y) => AdmonitionComponent(x, y, "tip"),
-            important: (x, y) => AdmonitionComponent(x, y, "important"),
-            caution: (x, y) => AdmonitionComponent(x, y, "caution"),
-            warning: (x, y) => AdmonitionComponent(x, y, "warning"),
-          },
-        },
-      ],
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "append",
-          properties: {
-            className: ["anchor"],
-          },
-          content: {
-            type: "element",
-            tagName: "span",
-            properties: {
-              className: ["anchor-icon"],
-              "data-pagefind-ignore": true,
-            },
-            children: [
-              {
-                type: "text",
-                value: "#",
-              },
-            ],
-          },
-        },
-      ],
-    ],
-  },
-  vite: {
-    build: {
-      rollupOptions: {
-        onwarn(warning, warn) {
-          if (
-            warning.message.includes("is dynamically imported by") &&
-            warning.message.includes("but also statically imported by")
-          ) {
-            return;
-          }
-          warn(warning);
-        },
-      },
-    },
-  },
-  prefetch: {
-    prefetchAll: true
-  },
-  image: {
-    service: {
-      config: {
-        limitInputPixels: false,
-      },
-    },
-    domains: ['blog-static.xeonzilla.top'],
-    experimentalLayout: "responsive",
-  },
-  i18n: {
-    locales: ["zh-cn"],
-    defaultLocale: "zh-cn",
-  },
-  experimental: {
-    responsiveImages: true,
-    clientPrerender: true,
-  },
+	site: "https://xeonzilla.top/",
+	base: "/",
+	trailingSlash: "always",
+	redirects: {
+		"/post/[...slug]": "/posts/[...slug]",
+	},
+	adapter: cloudflare(),
+	integrations: [
+		tailwind({
+			nesting: true,
+		}),
+		swup({
+			theme: false,
+			animationClass: "transition-swup-",
+			containers: ["main", "#toc"],
+			smoothScrolling: true,
+			cache: true,
+			preload: true,
+			accessibility: true,
+			updateHead: true,
+			updateBodyClass: false,
+			globalInstance: true,
+		}),
+		icon({
+			include: {
+				"preprocess: vitePreprocess(),": ["*"],
+				"fa6-brands": ["*"],
+				"fa6-regular": ["*"],
+				"fa6-solid": ["*"],
+			},
+		}),
+		svelte(),
+		sitemap(),
+	],
+	markdown: {
+		remarkPlugins: [
+			remarkMath,
+			remarkReadingTime,
+			remarkExcerpt,
+			remarkGithubAdmonitionsToDirectives,
+			remarkDirective,
+			remarkSectionize,
+			parseDirectiveNode,
+		],
+		rehypePlugins: [
+			rehypeKatex,
+			rehypeSlug,
+			[
+				rehypeComponents,
+				{
+					components: {
+						github: GithubCardComponent,
+						note: (x, y) => AdmonitionComponent(x, y, "note"),
+						tip: (x, y) => AdmonitionComponent(x, y, "tip"),
+						important: (x, y) => AdmonitionComponent(x, y, "important"),
+						caution: (x, y) => AdmonitionComponent(x, y, "caution"),
+						warning: (x, y) => AdmonitionComponent(x, y, "warning"),
+					},
+				},
+			],
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: "append",
+					properties: {
+						className: ["anchor"],
+					},
+					content: {
+						type: "element",
+						tagName: "span",
+						properties: {
+							className: ["anchor-icon"],
+							"data-pagefind-ignore": true,
+						},
+						children: [
+							{
+								type: "text",
+								value: "#",
+							},
+						],
+					},
+				},
+			],
+		],
+	},
+	vite: {
+		build: {
+			rollupOptions: {
+				onwarn(warning, warn) {
+					if (
+						warning.message.includes("is dynamically imported by") &&
+						warning.message.includes("but also statically imported by")
+					) {
+						return;
+					}
+					warn(warning);
+				},
+			},
+		},
+		ssr: {
+			external: ["node:path", "stream", "util"],
+		},
+	},
+	prefetch: {
+		prefetchAll: true,
+	},
+	image: {
+		service: {
+			config: {
+				limitInputPixels: false,
+			},
+		},
+		domains: ["blog-static.xeonzilla.top"],
+		experimentalLayout: "constrained",
+	},
+	i18n: {
+		locales: ["zh-cn"],
+		defaultLocale: "zh-cn",
+	},
+	experimental: {
+		responsiveImages: true,
+		clientPrerender: true,
+		contentIntellisense: true,
+		preserveScriptOrder: true,
+		headingIdCompat: true,
+	},
 });
