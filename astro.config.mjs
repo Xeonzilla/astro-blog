8f0ea9a8668e279507fb408770221d24371b8758
Xeonzilla <xeonrao01@gmail.com>
2025-08-05 22:06:02 +0800

    style: Import custom fonts to code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -76,7 +76,7 @@ export default defineConfig({
 				borderColor: "none",
 				codeFontSize: "0.875rem",
 				codeFontFamily:
-					"'JetBrains Mono Variable', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+					"'JetBrains Mono Variable', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', 'MiSans', monospace",
 				codeLineHeight: "1.5rem",
 				frames: {
 					editorBackground: "var(--codeblock-bg)",
