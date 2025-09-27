9f04b10eb77d0aad8f9e6d00a09eeeb0139cfdc2
Xeonzilla <xeonrao01@gmail.com>
2025-09-27 18:14:49 +0800

    refactor: Move friends list to separate friends-config.ts
    style: Refine Astro description

---
+++ src/friends-config.ts
@@ -0,0 +1,70 @@
+export interface Friend {
+	title: string;
+	siteurl: string;
+	imgurl: string;
+	desc?: string;
+}
+
+export const friendsLink: Friend[] = [
+	{
+		title: "Astro",
+		siteurl: "https://astro.build/",
+		imgurl: "https://avatars.githubusercontent.com/u/44914786",
+		desc: "The web framework for content-driven websites",
+	},
+	{
+		title: "Fuwari",
+		siteurl: "https://github.com/saicaca/fuwari",
+		imgurl:
+			"https://raw.githubusercontent.com/saicaca/resource/main/fuwari/home.png",
+		desc: "✨A static blog template built with Astro.",
+	},
+	{
+		title: "DailyMinz",
+		siteurl: "https://zh.dailyminz.org/",
+		imgurl: "https://zh.dailyminz.org/img/friends/stonepick.jpg",
+		desc: "Daily minds, daily mints.",
+	},
+	{
+		title: "Shadow3's Blog",
+		siteurl: "https://shadow3aaa.github.io/blog/",
+		imgurl: "https://avatars.githubusercontent.com/u/74781933",
+		desc: "🐷🐷🐷",
+	},
+	{
+		title: "Horean's Blog",
+		siteurl: "https://blog.hxrch.top/",
+		imgurl: "https://cdn-images.hxrch.top/blog-logo_128x128.png",
+		desc: "Spread the Knowledge Wisely & Widely",
+	},
+	{
+		title: "ZYBlog",
+		siteurl: "https://blog.pljzy.top/",
+		imgurl: "https://blog.pljzy.top/_astro/logo.BxIxyJV1_Z19cEQW.webp",
+		desc: "一个技术探索与分享的平台",
+	},
+	{
+		title: "Pinpe 的云端",
+		siteurl: "https://pinpe.top/",
+		imgurl: "https://pinpe.top/head.jpg",
+		desc: "一个属于自己的云朵。",
+	},
+	{
+		title: "SLHAF's blog",
+		siteurl: "https://blog.slhaf.work/",
+		imgurl: "https://blog.slhaf.work/upload/favicon.png",
+		desc: "SLHAF 的个人博客",
+	},
+	{
+		title: "橙树志 | citydatum",
+		siteurl: "https://citydatum.cn/",
+		imgurl: "https://citydatum.cn/wp-content/uploads/2024/12/citydatum-512.png",
+		desc: "过已过，来未来，唯当下",
+	},
+	{
+		title: "时歌的博客",
+		siteurl: "https://www.lapis.cafe/",
+		imgurl: "https://www.lapis.cafe/images/avatar.webp",
+		desc: "理解以真实为本，但真实本身并不会自动呈现",
+	},
+];
