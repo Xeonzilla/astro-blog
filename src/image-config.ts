ff74da98a77bca42e2ad7cd33439fc488c03cd92
Xeonzilla <xeonrao01@gmail.com>
2025-10-07 17:53:19 +0800

    feat: Responsive images for friends page

--- src/image-config.ts
+++ src/image-config.ts
@@ -30,3 +30,10 @@ export const animeImageConfig = {
 	sizes:
 		"(max-width: 767px) 84px, (max-width: 1023px) calc(17.5vw - 3px), (max-width: 1199px) calc(12.15vw - 5px), 150px",
 };
+
+export const friendsImageConfig = {
+	quality: "high",
+	widths: [125, 182, 256, 285, 364, 512],
+	sizes:
+		"(max-width: 639px) calc(50vw - 35px), (max-width: 767px) calc(33.333vw - 30px), (max-width: 1023px) calc((100vw - 136px) / 3), (max-width: 1199px) calc((100vw - 432px) / 3), (max-width: 1279px) 256px, 182px",
+};
