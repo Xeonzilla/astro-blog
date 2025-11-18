a8f0d3ed45daade5d1a87dabf9371f7f344c3baa
Xeonzilla <xeonrao01@gmail.com>
2025-11-18 18:50:07 +0800

    refactor: Move NAVBAR_HEIGHT to constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,7 +5,7 @@ export const LIGHT_MODE = "light",
 	AUTO_MODE = "auto";
 export const DEFAULT_THEME = AUTO_MODE;
 
-// Banner height unit: vh
+// Banner height, unit: vh
 export const BANNER_HEIGHT = 35;
 export const BANNER_HEIGHT_EXTEND = 30;
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
@@ -13,5 +13,8 @@ export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 // The height the main panel overlaps the banner, unit: rem
 export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5;
 
-// Page width: rem
+// Page width, unit: rem
 export const PAGE_WIDTH = 75;
+
+// Navbar height, unit: px
+export const NAVBAR_HEIGHT = 72;
